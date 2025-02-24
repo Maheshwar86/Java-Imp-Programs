16ef249eabd04f6e080cd657afe21d2825cf8431
Maheswar Reddy <maheswar.dtaoofficial@gmail.com>
2025-02-24 10:25:51 +0530

    count numbers added

--- CountNumbers.java
+++ CountNumbers.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CountNumbers {
+    public static void fun(int num){
+        int count=0;
+        while(num!=0){
+            num=num/10;
+            count++;
+        }
+        System.out.println("The Number Of Count IS : "+count);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = sc.nextInt();
+        fun(num);
+    }
 }
